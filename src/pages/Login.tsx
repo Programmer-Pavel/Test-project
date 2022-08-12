c70d1c9a44c09078a410cc4988e54e4fa09653e8
innodata <pavelkukuskmn388@gmail.com>
2022-08-12 15:23:49 +0300

    test husky

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,25 +2,6 @@
 
 export const Login = () => {
   var removeElement = function (nums: any[]) {
-    // let tt: any = [];
-    // let copyArr = [...nums];
-
-    // nums.forEach((item, i) => {
-    //   const test = nums.filter((element) => element === item);
-
-    //   console.log('test', test);
-
-    //   if (tt.includes(item)) {
-    //     copyArr.splice(i + tt.length, test.length);
-    //   } else {
-    //     tt.push(item);
-    //   }
-    // });
-
-    // console.log('copyArr', copyArr);
-
-    // let tt: any = new Set([...nums]);
-
     nums.splice(0, nums.length, ...Array.from(new Set([...nums])));
 
     console.log('nums', nums);
